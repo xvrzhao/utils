35500808466c0bb9968e095f8d89b6fe872dd123
Xavier Zhao <xvrzhao@gmail.com>
2020-01-02 16:34:03 +0800

    add: db/gorm

---
+++ db/gorm/open.go
@@ -0,0 +1,17 @@
+package gorm
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"net/url"
+)
+
+func Open(dbHost, dbPort, dbName, username, password string) (db *gorm.DB, err error) {
+	dsn := fmt.Sprintf("%s:%s@(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=%s",
+		username, password, dbHost, dbPort, dbName, url.QueryEscape("Asia/Shanghai"))
+	db, err = gorm.Open("mysql", dsn)
+	if err != nil {
+		return
+	}
+	return
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module code.aliyun.com/microstack/utils
+
+go 1.13
+
+require github.com/jinzhu/gorm v1.9.11 // indirect
