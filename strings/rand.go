f6ae1c329ed9a3c80e4cd814bac4d61284392303
Xavier Zhao <xvrzhao@gmail.com>
2020-01-14 17:46:47 +0800

    add: RandNum function

--- strings/rand.go
+++ strings/rand.go
@@ -5,30 +5,47 @@ import (
 	"time"
 )
 
-// RandLetterNum 生成包含大小写字母和数字的随机字符串
-func RandLetterNum(len int) string {
-	const (
-		numLetters    = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-		numLettersLen = 62
-		bits          = 6        // 62 的二进制位数，111110 共 6 位
-		mask          = 0b111111 // 6 位掩码
-		times         = 10       // 生成一个 63 位随机数，每次取 6 位，可以取 10 次
-	)
+const (
+	numbers    = "0123456789"
+	numLetters = numbers + "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+)
 
+// RandNum 生成长度为 len 的数字字符串。
+func RandNum(len int) string {
 	res := make([]byte, len)
-	rand.Seed(time.Now().UnixNano())
-	asset, remain := rand.Int63(), times
+	asset, remain := rand.Uint64(), 16
 
 	for i := 0; i < len; i++ {
 		if remain == 0 {
-			asset, remain = rand.Int63(), times
+			asset, remain = rand.Uint64(), 16
 		}
-		if bits6 := asset & mask; bits6 < numLettersLen {
+		if bits4 := asset & 0b1111; bits4 < 10 {
+			res[i] = numbers[bits4]
+		} else {
+			i--
+		}
+		asset >>= 4
+		remain--
+	}
+
+	return string(res)
+}
+
+// RandLetterNum 生成包含大小写字母和数字的随机字符串。
+func RandLetterNum(len int) string {
+	res := make([]byte, len)
+	asset, remain := rand.Int63(), 10
+
+	for i := 0; i < len; i++ {
+		if remain == 0 {
+			asset, remain = rand.Int63(), 10
+		}
+		if bits6 := asset & 0b111111; bits6 < 62 {
 			res[i] = numLetters[bits6]
 		} else {
 			i--
 		}
-		asset >>= bits
+		asset >>= 6
 		remain--
 	}
 
--- strings/rand_test.go
+++ strings/rand_test.go
@@ -28,3 +28,28 @@ func BenchmarkRandLetterNum(b *testing.B) {
 		RandLetterNum(10)
 	}
 }
+
+func TestRandNum(t *testing.T) {
+	n := 6
+	a := RandNum(n)
+	b := RandNum(n)
+	c := RandNum(n)
+
+	t.Logf("%q %q %q", a, b, c)
+
+	if len(a) != n || len(b) != n || len(c) != n {
+		t.Fail()
+		return
+	}
+	if a == b || a == c || b == c {
+		t.Fail()
+		return
+	}
+}
+
+func BenchmarkRandNum(b *testing.B) {
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		RandNum(10)
+	}
+}
