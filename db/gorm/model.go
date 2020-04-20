2907ae4331cc089f476914fc2cb964057ca9e6b1
Xavier Zhao <xvrzhao@gmail.com>
2020-04-20 15:46:49 +0800

    mod: gorm.Model add json tag

--- db/gorm/model.go
+++ db/gorm/model.go
@@ -2,8 +2,8 @@ package gorm
 
 // Model 用来替代 gorm.Model，时间字段的类型均为 Time，而非 time.Time。
 type Model struct {
-	ID        uint `gorm:"primary_key"`
-	CreatedAt Time
-	UpdatedAt Time
-	DeletedAt *Time `sql:"index"`
+	ID        uint  `gorm:"primary_key" json:"id"`
+	CreatedAt Time  `json:"createdAt"`
+	UpdatedAt Time  `json:"updatedAt"`
+	DeletedAt *Time `sql:"index" json:"-"`
 }
