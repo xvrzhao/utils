273a3e95a25ffac9218484a243db98fd65845263
Xavier Zhao <xvrzhao@gmail.com>
2020-06-10 11:42:10 +0800

    mod: GetPagePerFromQs: increase condition judgment

--- http/gin/page.go
+++ http/gin/page.go
@@ -9,15 +9,15 @@ import (
 // The prerequisite for using this function is that the parameters' name of the page number and the number of items
 // per page in query string must be `page` and `per`, like `example.com?page=2&per=10`.
 //
-// If the page parameter in query string is not a number, the returned page will be 1. And if the per parameter
-// in query string is not a number, the returned per will be defaultPer, if the requested per is larger than
-// maxPer, the returned per will be maxPer.
+// If the page parameter in query string is not a number or less than 1, the returned page will be 1. If the per
+// parameter in query string is not a number, the returned per will be defaultPer, and if the requested per is greater
+// than maxPer, the returned per will be maxPer.
 func GetPagePerFromQs(c *gin.Context, defaultPer, maxPer int) (page, per int) {
 	pageStr := c.Query("page")
 	perStr := c.Query("per")
 
 	page, err := strconv.Atoi(pageStr)
-	if err != nil {
+	if err != nil || page < 1 {
 		page = 1
 	}
 
